69eff254a43ae70cbdfc3feb929156b2948b000e
abida-03 <abida.bin03@gmail.com>
2025-09-29 09:59:13 +0600

    first commit

---
+++ pizza_bill_calculator.py
@@ -0,0 +1,25 @@
+print("Welcome to the Python Pizza Deliveries!")
+size = str(input("What size pizza do you want? S, M or L: ")).upper()
+pepperoni = str(input("Do you want pepperoni in your pizza? Y or N: ")).upper()
+extra_cheese = str(input("Do you want extra cheese? Y or N: ")).upper()
+bill = 0
+
+if size == "S":
+    bill = 15
+    if pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill = 20
+    if pepperoni == "Y":
+        bill += 3
+elif size == "L":
+    bill = 25
+    if pepperoni == "Y":
+        bill += 3
+else:
+    print("Wrong input ")
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f'Your final bill is ${bill}')
